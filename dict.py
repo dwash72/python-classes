33e0d8b3acbf7177526a2a55c35934eb7c4d5ec7
dwash <dwash@email.com>
2021-04-27 09:12:35 +0530

    start

---
+++ dict.py
@@ -0,0 +1,16 @@
+# d = {"name":1, "age":20}
+# # print(d["s"])
+# # print(d.get("name"))
+# # print(d.update(age=30))
+# print(d.get("m"), "not found")
+
+#for loop
+
+d = {"name":"dwash", "address":"purasaiwalkam"}
+empty = {}
+for d,v in d.items():
+	empty[d] = v
+print(empty)
+
+
+	
