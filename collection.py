aa154cae3709c576547a908ff1d321b02713eae7
dwash <dwash@email.com>
2021-04-27 19:07:56 +0530

    start

--- collection.py
+++ collection.py
@@ -1,6 +1,16 @@
-import collections
-l = ["a","b","c","d","d"]
-print(help(collections.Counter))
+# import collections
+# l = ["a","b","c","d","d"]
+# print(collections.Counter(l))
 
+#for loop
 
-
+l1 = ["1","2","3","4","5","6","7","8","9","10","10"]
+d = {}
+for x in l1:
+ # print(x)
+	if x in d:
+		d[x] and d[x] + 1
+# print(x)
+	else:
+		d[x] = 1
+print(d)
