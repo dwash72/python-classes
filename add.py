222b436037e01fe7d6bc572e802a266318dd86c7
dwash <dwash@email.com>
2021-04-29 09:07:00 +0530

    start

--- add.py
+++ add.py
@@ -1,13 +1,13 @@
 # for adding numbers in a same list
 l1 = [1,2,3,4]
-# empty = 0
-# for x in range(len(l1)):
-# 	empty = empty + l1[x]
-# 	# if s in v:
-# 	# 	v[s] and v[s] + 1
-# print(empty)
+empty = 0
+for x in range(len(l1)):
+	empty = empty + l1[x]
+	# if s in v:
+	# 	v[s] and v[s] + 1
+print(empty)
 
-print(dir(str))
+# print(dir(str))
 
-# empty or not 
-use find method in str 
+# # empty or not 
+# use find method in str
--- squar.py
+++ squar.py
@@ -1,15 +1,36 @@
-# # square of the list 
+# # # square of the list 
 # l1 = [1,2,3,4,5]
 # for x in l1:
 # 	# print(x)
-# 	square = [x ** 2]
-# print(square)
+#  print(x**2)
+
 
 
 # # ssorting a list (assending)
 # smallest = [1,2,3,5,7,9,8]
 # print(smallest.sort())
 
-# smallest in the list
-l1 = [0,1,2,3,4,56,]
-print(max(l1))
+# # smallest in the list
+# l1 = [0,1,2,3,4,56,]
+# print(max(l1))
+
+#break
+
+# l1 = [1,2,3,4,5]
+# for v in l1:
+# 	if v == 4:
+# 		continue
+# 	else:
+# 	 	print(v)
+
+user_name = input("enter your name:")
+user_age = int(input("enter your age:"))
+l1 = list(range(1, 100))
+l2 = ["dwash", "vandhana" ,"raju" ,"cho","lokesh"]
+for user_name in l2:
+	break
+else:
+	print(user_name)
+
+
+
