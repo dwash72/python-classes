34f7913e389dcb1182ca0386347a57180e1316e8
dwash <dwash@email.com>
2021-05-06 09:02:33 +0530

    start

--- fy.py
+++ fy.py
@@ -24,14 +24,35 @@
 # 		print(data, w[data])
 # fu(w)
 
+# multiple table
 # def multiple (num, multiple):
 # 	for x in range(1, num + 1):
 # 		print(x * multiple)
 # multiple(10, 5)
 
-def mul(num,m):
-	value = []
-	for x in range(1,num + 1):
-		value.append(x * m)
-	return value
-print(mul(10, 5))
+# def mul(num,m):
+# 	value = []
+# 	for x in range(1,num + 1):
+# 		value.append(x * m)
+# 	return value
+# print(mul(10, 5))
+
+# def multi(n):
+# 	for x in range(1,11):
+# 		print(n, x, n * x)
+# n = 10
+# multi(n)
+
+# def function(m, s):
+# 	data = [d * s for d in range(1 , m + 1)]
+# 	return data     
+# print(function(10, 5))		
+
+# def function(*args , **kwargs):
+# 	print(args , kwargs)
+# function(10,2,3 , a=10, b = 20)
+           
+def function(e):
+	return e * e
+data = list(map(function,[1,2,3,4]))
+print(data)
