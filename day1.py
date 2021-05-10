44ce5d412bdb7b28f433d272d7e9d54ffe1f3b73
dwash <dwash@email.com>
2021-05-10 13:25:25 +0530

    start

---
+++ day1.py
@@ -0,0 +1,66 @@
+
+# import re
+# data = "hello"
+# r = re.search("he", data)
+# if r :
+# 	print("yes")
+# else:
+# 	print("no")
+
+# import re
+# data = "hello"
+# x = re.findall("hi", data)
+# if x :
+# 	print("yes")
+# else:
+# 	print("no")
+
+# import re
+# data = "hello"
+# x = re.match("hi", data)
+# if x :
+# 	print("yes")
+# else:
+# 	print("no")
+
+# import re
+# data = "hello"
+# x = re.findall("hi", data)
+# if x :
+# 	print("yes")
+# else:
+# 	print("no")
+
+# import re
+# data = "hello"
+# x = re.findall("hello", data)
+# if x :
+# 	print("yes")
+# else:
+# 	print("no")
+
+#method function
+# def function(strrrr , dwash):
+# 	if (strrrr.find(dwash) == -1):
+# 		print("no")
+# 	else:
+# 		print("yes")
+# strrrrr = "vins"
+# dwash = ""
+# function(strrrrr, dwash)
+
+#excercise-2
+
+# def function(composers):
+# 	for x in composers:
+# 		x = sorted(composers)
+# composers = {'johann':52, 'k':22}
+# function(composers)
+
+# def  dic():
+# 	composers = {'johann':52, 'k':22}
+# 	for x in sorted(composers):
+# 		print((i , composers[i]), end = "")
+# def main():
+# 	dic()
+# 	print(main())
