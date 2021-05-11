ecbb399eb41b4e4a2345efbdd7ff71f93a98b8ed
dwash <dwash@email.com>
2021-05-11 19:23:53 +0530

    s

---
+++ day2.py
@@ -0,0 +1,9 @@
+d1 = {'d':1, 'c':2, 'b':5}
+d2 = {'c':2, 'b':100}
+intersect = (d1.keys() & d2.keys())
+print(str(intersect))
+
+# def intersect(d1, d2):
+# 	return (d1.items() & d2.items())
+# print(intersect({'a':2,'c':10,'f':20},{'b':42, 'a':2,'c':10}))
+
