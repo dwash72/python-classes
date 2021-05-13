b6bf7dbbfcc0821eda20cfa95452f46b24fca1ab
dwash <dwash@email.com>
2021-05-13 12:20:17 +0530

    s

--- day2.py
+++ day2.py
@@ -1,7 +1,8 @@
-d1 = {'d':1, 'c':2, 'b':5}
-d2 = {'c':2, 'b':100}
-intersect = (d1.keys() & d2.keys())
-print(str(intersect))
+def function(d1 , d2):
+	intersect = (d1.keys() & d2.keys())
+	di = {key:(d1.get(key),d2.get(key)) for key in intersect}
+	print(di)
+function({'d':1, 'c':2, 'b':5},{'c':2, 'b':100})
 
 # def intersect(d1, d2):
 # 	return (d1.items() & d2.items())
