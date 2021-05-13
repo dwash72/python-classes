0633a9064d515dc3894e4ee63a91d2e20b215ff7
dwash <dwash@email.com>
2021-05-13 17:56:44 +0530

    s

---
+++ day3.py
@@ -0,0 +1,20 @@
+# import itertools
+# data_list = {'country':'usa', 'state':'colorona', 'name':'ariyana'},
+# {'country':'usa', 'state':'missouri', 'name':'michelle'},{'country':'india', 'state':'co', 'name':'ar'}
+# x = itertools.groupby(data_list)
+# for k,g in x:
+# 	print(k)
+# 	print(list(g))
+
+from itertools import groupby
+data_list = [
+{"country":"usa", "state":"olorona", "name":"ariyana"},
+{"country":"usa", "state":"missouri", "name":"michelle"}, 
+{'country':"india", 'state':"co", 'name':"ariyana"},{'country':"india", 'state':"chennai", 'name':"deag"},
+]
+def function(k):
+	return k['country']
+data_list = sorted(data_list,key=  function)
+for key,value in groupby(data_list,function):
+	print(key)
+	print(list(value))
