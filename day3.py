c714cff73f806c1742624dc1fef39603e8d3351e
dwash <dwash@email.com>
2021-05-21 13:59:57 +0530

    s

--- day3.py
+++ day3.py
@@ -8,13 +8,33 @@
 
 from itertools import groupby
 data_list = [
-{"country":"usa", "state":"olorona", "name":"ariyana"},
-{"country":"usa", "state":"missouri", "name":"michelle"}, 
-{'country':"india", 'state':"co", 'name':"ariyana"},{'country':"india", 'state':"chennai", 'name':"deag"},
+{
+"country":"usa",
+"state":"olorona", 
+"name":"ariyana"
+},
+{
+"country":"usa", 
+"state":"missouri", 
+"name":"michelle"
+}, 
+{
+'country':"india", 
+'state':"co", 
+'name':"ariyana"
+},
+{
+'country':"india",
+ 'state':"chennai", 
+ 'name':"deag"
+ },
 ]
 def function(k):
 	return k['country']
 data_list = sorted(data_list,key=  function)
 for key,value in groupby(data_list,function):
 	print(key)
-	print(list(value))
+	# print(list(value))
+	# print(list(value))
+	for data in value:
+		print(data)
---
+++ day4.py
@@ -0,0 +1,24 @@
+# def findArea(r):
+# 	PI = 3.142
+# 	return PI * (r*r);
+# print(findArea(5))
+# Driver method
+# print(“Area is %.6f ” % findArea(5));
+
+# def function(s):
+# 	s = 0
+# 	for x in range(1, n+1):
+# 		s= s + (i * i)
+# 	return s
+# 	n = 4
+# print(function(s))
+
+ex = ['one.json','two.rst','j.rst']
+newlist = {}
+for x in ex:
+	if '.rst' in x:
+		newlist[x] = True
+	else:
+		newlist[x] = False
+print(newlist)
+	
